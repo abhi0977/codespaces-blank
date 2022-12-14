ddeaf33a26bb12b13253f8579e30399c41aeac8a
Abhi Ramani <abhiramani3@gmail.com>
2022-12-14 04:23:26 +0000

    feat: initial commit

---
+++ src/knight/core.clj
@@ -0,0 +1,11 @@
+(ns knight.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+   (println "I'm a little teapot!"))
