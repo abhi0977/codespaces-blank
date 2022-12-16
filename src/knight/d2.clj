62d4bdf0113907494ddb31c38f6ea27f538be420
Abhi Ramani <abhiramani3@gmail.com>
2022-12-16 17:38:51 +0000

    done: d2 p2

--- src/knight/d2.clj
+++ src/knight/d2.clj
@@ -42,3 +42,51 @@
   (reduce + (map total-points normalized-input)))
 
 (problem-1)
+
+;; p2
+(def expected-result-map {"X" :lose
+                          "Y" :draw
+                          "Z" :win})
+
+(defn lose [str]
+  (cond
+    (= str :rock) :scissors
+    (= str :paper) :rock
+    (= str :scissors) :paper))
+
+(defn win [str]
+  (cond
+    (= str :rock) :paper
+    (= str :paper) :scissors
+    (= str :scissors) :rock))
+
+
+(def reverse-map {:rock "X"
+                  :paper "Y"
+                  :scissors "Z"})
+
+
+(defn draw [str] str)
+
+(defn convert [strategy]
+  (def temp (clojure.string/split strategy #" "))
+
+  (def opp-st (-> (first temp) (config)))
+  (def expected-result-enc (-> (last temp)))
+
+  (def res (expected-result-map expected-result-enc))
+  (def res (cond
+             (= res :win) (win opp-st)
+             (= res :lose) (lose opp-st)
+             :else (draw opp-st)))
+  (str (first temp) " " (reverse-map res)))
+
+
+(defn problem-2 []
+  (def normalized-input
+    (-> "input.txt"
+        (slurp)
+        (clojure.string/split-lines)))
+  (reduce + (map total-points (map convert normalized-input))))
+
+(problem-2)
